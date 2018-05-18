19119725917fc143622dfbf9ae13e1b7747e983f
Andela Developer <tosmak16@gmail.com>
2018-05-18 18:08:37 +0100

    write first test

--- blog_app/tests.py
+++ blog_app/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+client = Client()
+
+
+class QuickApiTest(TestCase):
+
+    def test_index_view_returns_a_response_message(self):
+        """
+        checks if index_view returns a response
+        message when get request is sent
+        """
+
+        response = client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello welcome to my blog')
+

1b29dbdd4f66fc879862c4b7e989ae23e0942500
Andela Developer <tosmak16@gmail.com>
2018-05-25 15:38:18 +0100

    add post rating model - refactor user controller - refactor post controller

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
 from django.contrib.auth.hashers import make_password
 from django.utils import timezone
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, username, display_name=None, password=None):
@@ -67,4 +68,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rating_owner')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='user_post')
+    score = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f'{self.post.title} {self.user.id} {self.score}'
+
 
--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -1,19 +1,50 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueTogetherValidator
+from django.db.models import Avg
 
-from .models import User, Post
+
+from .models import Post, Rating
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'password', 'url')
-    url = serializers.HyperlinkedIdentityField(view_name='user-detail', lookup_field='id')
+        fields = ('id', 'username', 'email', 'password',)
 
 
 class PostSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'body', 'user', 'url', 'created_date', 'published_date', 'is_published')
+        fields = ('id', 'title', 'body', 'user', 'url', 'created_date',
+                  'published_date', 'is_published', 'average_rating',)
+
+    @classmethod
+    def get_average_rating(cls, post_object):
+        average = Rating.objects.filter(post=post_object).aggregate(Avg('score')).get('score__avg')
+
+        if average is None:
+            return 0
+        return round(average*2)/2
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    validators = [
+        UniqueTogetherValidator(
+            queryset=Rating.objects.all(),
+            fields=('user', 'post')
+        )
+    ]
+
+    @classmethod
+    def validate_score(cls, score_data):
+        if score_data > 5 or score_data < 1:
+            raise serializers.ValidationError("score should be between 1 and 5")
+        return score_data
+
+    class Meta:
+        model = Rating
+        fields = ('id', 'score', 'user', 'post', 'created_at')
